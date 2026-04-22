b2c37fc7774c05cb02936c0cc59fac1b4258eb3a
Federico Bruzzone <federico.bruzzone.i@gmail.com>
2026-04-22 16:16:07 +0200

    refactor: remove unnecessary comments in Version.cpp for clarity
    Signed-off-by: Federico Bruzzone <federico.bruzzone.i@gmail.com>

--- lib/Driver/Version.cpp
+++ lib/Driver/Version.cpp
@@ -16,9 +16,6 @@ namespace {
 
 constexpr Version CurrentVersion(0, 1, 0);
 
-// We store the formatted string in a static buffer so that the
-// std::string_view returned by version() remains valid for the lifetime
-// of the program.
 const std::string CurrentVersionStr =
     std::to_string(CurrentVersion.Major) + "." +
     std::to_string(CurrentVersion.Minor) + "." +
@@ -26,10 +23,6 @@ const std::string CurrentVersionStr =
 
 } // namespace
 
-[[nodiscard]] std::string_view version() noexcept {
-  // Returning a string_view is O(1) and avoids any heap allocation
-  // during the function call.
-  return CurrentVersionStr;
-}
+[[nodiscard]] std::string_view version() noexcept { return CurrentVersionStr; }
 
 } // namespace eter
