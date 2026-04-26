071471d8a88c31f76f24e3822a1cb1fe92b43bf8
Marantz <riccardo.marantonio@gmail.com>
2026-04-26 22:18:07 +0200

    feat: Implementation of the lexer.h structure

--- include/eter/Lexer/Lexer.h
+++ include/eter/Lexer/Lexer.h
@@ -9,6 +9,34 @@
 #ifndef ETER_LEXER_LEXER_H
 #define ETER_LEXER_LEXER_H
 
-namespace eter::lex {} // namespace eter::lex
+#include "eter/Base/DiagnosticEngine.h"
+#include "eter/Lexer/Token.h"
+
+#include <llvm/ADT/StringRef.h>
+
+namespace eter::lex {
+class Lexer {
+public:
+  Lexer(llvm::StringRef SourceBuffer, DiagnosticEngine &Diagnostics)
+      : SourceBuffer(SourceBuffer), CurrentPosition(SourceBuffer.begin()),
+        Diagnostics(Diagnostics) {}
+
+  void Lex(Token &Result) {};
+
+private:
+  /// Consumes whitespace and comments (handling nested /* */ as per spec).
+  void skipWhitespaceAndComments();
+
+  /// Specialized lexing routines for different token categories.
+  void lexIdentifier(Token &Result, const char *TokStart);
+  void lexNumericLiteral(Token &Result, const char *TokStart);
+  void lexStringLiteral(Token &Result, const char *TokStart,
+                        bool IsRaw = false);
+
+  llvm::StringRef SourceBuffer;
+  const char *CurrentPosition;
+  DiagnosticEngine &Diagnostics;
+};
+} // namespace eter::lex
 
 #endif // ETER_LEXER_LEXER_H
