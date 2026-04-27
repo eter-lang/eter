0b834f911036fc67f5eed8434e3760d61c177d6d
Marantz <riccardo.marantonio@gmail.com>
2026-04-27 12:50:44 +0200

    Implemented .h and .cpp for Token

--- include/eter/Lexer/Token.h
+++ include/eter/Lexer/Token.h
@@ -9,35 +9,32 @@
 #ifndef ETER_LEXER_TOKEN_H
 #define ETER_LEXER_TOKEN_H
 
-#include <string_view>
+#include <llvm/ADT/StringRef.h>
 
-namespace eter::lex {
+namespace eter::lexer {
 
 /// Represents a single token produced by the lexer.
 struct Token {
   /// The kind of token (identifier, number, string, keyword, symbol, etc.)
-  enum class Kind {
-    Identifier,
-    Number,
-    String,
-    Keyword,
-    Symbol,
-    EndOfFile,
-    Unknown
+  enum class Kind : uint16_t {
+#define TOK(X) X,
+#include "eter/Lexer/TokenKinds.def"
+#undef TOK
   };
 
   /// The kind of the token.
   Kind TokenKind;
   /// The actual text of the token as it appears in the source code.
-  std::string_view Lexeme;
+  llvm::StringRef Lexeme;
   /// The position of the token in the source code (e.g., byte offset).
   uint32_t StartPosition;
 
-  explicit Token(Kind TokenKind, std::string_view Lexeme,
-                 uint32_t StartPosition)
+  explicit Token(Kind TokenKind, llvm::StringRef Lexeme, uint32_t StartPosition)
       : TokenKind(TokenKind), Lexeme(Lexeme), StartPosition(StartPosition) {}
+
+  static const char *getTokenName(Kind K);
 };
 
-} // namespace eter::lex
+} // namespace eter::lexer
 
 #endif // ETER_LEXER_TOKEN_H
